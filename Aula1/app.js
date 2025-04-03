3aa309021df11e36bfcb9aa74bc073cc2ad4fa59
giparreira <gi.parreirinha@gmail.com>
2025-04-03 20:33:56 -0300

    testando

---
+++ Aula1/app.js
@@ -0,0 +1,11 @@
+// alert("Hello world");
+// var 
+// let, valor que muda, pode ser reatribuido
+// const, valor fixo
+let nome = prompt("Qual é o seu nome?", "Insira aqui o seu nome")
+let idade = prompt("Qual a sua idade?")
+let teste = null //vazio intencional
+//typeof para descobrir o tipo da variavel
+console.log("mensagem");
+console.log(nome)
+console.log(idade)
