41a03d3af5e4bc1927b895835362414e9f6b0069
giparreira <gi.parreirinha@gmail.com>
2025-04-03 20:56:13 -0300

    alert e prompt

--- Aula1/app.js
+++ Aula1/app.js
@@ -2,10 +2,20 @@
 // var 
 // let, valor que muda, pode ser reatribuido
 // const, valor fixo
-let nome = prompt("Qual é o seu nome?", "Insira aqui o seu nome")
-let idade = prompt("Qual a sua idade?")
-let teste = null //vazio intencional
+
+// let nome = prompt("Qual é o seu nome?", "Insira aqui o seu nome")
+// let idade = prompt("Qual a sua idade?")
+// let teste = null //vazio intencional
+// let trabalho = prompt("Você trabaha com o que?")
+let n1 = Number(prompt("Adicione um número"))
+let n2 = Number(prompt("Adicione outro número"))
+resultado = n1 + n2
+alert(resultado)
+
+alert(`Olá ${nome}, você tem ${idade} anos e trabalha como ${trabalho}`)
+
 //typeof para descobrir o tipo da variavel
+
 console.log("mensagem");
 console.log(nome)
 console.log(idade)
